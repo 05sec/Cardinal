5a4e0ca7e2dac281198c7484a79fecb6f0cd9f42
John Wu <524306184@qq.com>
2020-03-02 13:57:46 +0800

    feat: install i18n

--- src/install.go
+++ src/install.go
@@ -97,7 +97,7 @@ func (s *Service) install() {
 			InputString(&index, "type 1, 2... to select")
 		}
 
-		content, err := s.GenerateConfigFileGuide()
+		content, err := s.GenerateConfigFileGuide(languages[index])
 		if err != nil {
 			log.Fatalln(err)
 		}
@@ -105,11 +105,12 @@ func (s *Service) install() {
 		if err != nil {
 			log.Fatalln(err)
 		}
+		log.Println(s.I18n.T(languages[index], "install.create_config_success"))
 	}
 }
 
 // GenerateConfigFileGuide can lead the user to fill in the config file.
-func (s *Service) GenerateConfigFileGuide() ([]byte, error) {
+func (s *Service) GenerateConfigFileGuide(lang string) ([]byte, error) {
 	input := struct {
 		Title, BeginTime, RestTime, EndTime, Duration, Port, Salt, FlagPrefix, FlagSuffix, CheckDownScore, AttackScore, DBHost, DBUsername, DBPassword, DBName string
 	}{
@@ -123,14 +124,14 @@ func (s *Service) GenerateConfigFileGuide() ([]byte, error) {
 		DBName:         "cardinal",
 	}
 
-	log.Println("Cardinal.toml 配置文件不存在，安装向导将带领您进行配置。")
+	log.Println(s.I18n.T(lang, "install.greet"))
 
-	InputString(&input.Title, "请输入比赛名称")
+	InputString(&input.Title, string(s.I18n.T(lang, "install.input_title")))
 
 	var beginTime time.Time
 	err := errors.New("")
 	for err != nil {
-		InputString(&input.BeginTime, "请输入比赛开始时间（格式 2020-02-17 12:00:00）")
+		InputString(&input.BeginTime, string(s.I18n.T(lang, "install.begin_time")))
 		beginTime, err = time.ParseInLocation("2006-01-02 15:04:05", input.BeginTime, time.Local)
 	}
 	input.BeginTime = beginTime.Format(time.RFC3339)
@@ -138,21 +139,21 @@ func (s *Service) GenerateConfigFileGuide() ([]byte, error) {
 	var endTime time.Time
 	err = errors.New("")
 	for err != nil {
-		InputString(&input.EndTime, "请输入比赛结束时间（格式 2020-02-17 12:00:00）")
+		InputString(&input.EndTime, string(s.I18n.T(lang, "install.end_time")))
 		endTime, err = time.ParseInLocation("2006-01-02 15:04:05 ", input.EndTime, time.Local)
 	}
 	input.EndTime = endTime.Format(time.RFC3339)
 
-	InputString(&input.Duration, "请输入每轮长度（单位：分钟，默认值：2）")
-	InputString(&input.Port, "请输入后端服务器端口号（默认值：19999）")
-	InputString(&input.FlagPrefix, "请输入 Flag 前缀（默认值：hctf{）")
-	InputString(&input.FlagSuffix, "请输入 Flag 后缀（默认值：}）")
-	InputString(&input.CheckDownScore, "请输入每次 Checkdown 扣分（默认值：50）")
-	InputString(&input.AttackScore, "请输入每次攻击得分（默认值：50）")
-	InputString(&input.DBHost, "请输入数据库地址（默认值：localhost:3306）")
-	InputString(&input.DBUsername, "请输入数据库账号：")
-	InputString(&input.DBPassword, "请输入数据库密码：")
-	InputString(&input.DBName, "请输入数据库表名（默认值：cardinal）")
+	InputString(&input.Duration, string(s.I18n.T(lang, "install.duration")))
+	InputString(&input.Port, string(s.I18n.T(lang, "install.port")))
+	InputString(&input.FlagPrefix, string(s.I18n.T(lang, "install.flag_prefix")))
+	InputString(&input.FlagSuffix, string(s.I18n.T(lang, "install.flag_suffix")))
+	InputString(&input.CheckDownScore, string(s.I18n.T(lang, "install.checkdown_score")))
+	InputString(&input.AttackScore, string(s.I18n.T(lang, "install.attack_score")))
+	InputString(&input.DBHost, string(s.I18n.T(lang, "install.db_host")))
+	InputString(&input.DBUsername, string(s.I18n.T(lang, "install.db_username")))
+	InputString(&input.DBPassword, string(s.I18n.T(lang, "install.db_password")))
+	InputString(&input.DBName, string(s.I18n.T(lang, "install.db_name")))
 
 	// Generate Salt
 	input.Salt = randstr.String(64)
@@ -175,13 +176,13 @@ func (s *Service) initManager() {
 	if managerCount == 0 {
 		// Create manager account if managers table is empty.
 		var managerName, managerPassword string
-		InputString(&managerName, "请输入管理员账号：")
-		InputString(&managerPassword, "请输入管理员密码：")
+		InputString(&managerName, string(s.I18n.T(s.Conf.Base.SystemLanguage, "install.manager_name")))
+		InputString(&managerPassword, string(s.I18n.T(s.Conf.Base.SystemLanguage, "install.manager_password")))
 		s.Mysql.Create(&Manager{
 			Name:     managerName,
 			Password: s.addSalt(managerPassword),
 		})
-		s.NewLog(WARNING, "system", fmt.Sprintf("添加管理员账号成功，请妥善保管您的账号密码信息！"))
-		log.Println("添加管理员账号成功")
+		s.NewLog(WARNING, "system", string(s.I18n.T(s.Conf.Base.SystemLanguage, "install.manager_success")))
+		log.Println(s.I18n.T(s.Conf.Base.SystemLanguage, "install.manager_success"))
 	}
 }
