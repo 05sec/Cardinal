b6246a39c0bb70a4c759febe3aa68354f86e2035
E99p1ant <i@github.red>
2020-09-07 23:18:45 +0800

    feat(docker): stop container

--- internal/container/container.go
+++ internal/container/container.go
@@ -6,6 +6,7 @@ import (
 	"github.com/docker/docker/client"
 	"github.com/gin-gonic/gin"
 	"github.com/vidar-team/Cardinal/internal/utils"
+	"time"
 )
 
 // GetContainerList returns all the docker containers.
@@ -42,4 +43,28 @@ func StartContainer(c *gin.Context) (int, interface{}) {
 	}
 
 	return utils.MakeSuccessJSON("启动成功")
+}
+
+// StopContainer starts the container.
+func StopContainer(c *gin.Context) (int, interface{}) {
+	var inputForm struct {
+		ContainerId string `binding:"required"`
+	}
+	err := c.ShouldBind(&inputForm)
+	if err != nil {
+		return utils.MakeErrJSON(400, 40099, "payload error")
+	}
+
+	containerID := inputForm.ContainerId
+	cli, err := client.NewClientWithOpts(client.WithAPIVersionNegotiation())
+	if err != nil {
+		return utils.MakeErrJSON(500, 59999, err)
+	}
+	timeout := 15 * time.Second
+	err = cli.ContainerStop(context.Background(), containerID, &timeout)
+	if err != nil {
+		return utils.MakeErrJSON(500, 59999, err)
+	}
+
+	return utils.MakeSuccessJSON("关闭成功")
 }
--- internal/route/router.go
+++ internal/route/router.go
@@ -171,6 +171,7 @@ func Init() *gin.Engine {
 		// Docker
 		managerRouter.GET("/docker/containers", __(container.GetContainerList))
 		managerRouter.POST("/docker/container/start", __(container.StartContainer))
+		managerRouter.POST("/docker/container/stop", __(container.StopContainer))
 		managerRouter.POST("/docker/findImage", __(container.GetImageData))
 
 		// Log
