611032942b0632d7c99d34c612078911a0885654
E99p1ant <i@github.red>
2020-12-19 15:04:42 +0800

    tidy: remove useless warp

--- internal/route/wrapper.go
+++ internal/route/wrapper.go
@@ -2,12 +2,7 @@ package route
 
 import "github.com/gin-gonic/gin"
 
-//func __(code int, obj interface{}) func(*gin.Context) {
-//	return func(c *gin.Context) {
-//		c.JSON(code, obj)
-//	}
-//}
-
+// __ is the wrap of the Gin handler.
 func __(handler func(*gin.Context) (int, interface{})) func(*gin.Context) {
 	return func(c *gin.Context) {
 		c.JSON(handler(c))
