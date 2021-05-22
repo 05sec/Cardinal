0051d25c0e963e5409569a69dc9d48562ada047c
E99p1ant <i@github.red>
2021-05-22 14:19:20 +0800

    refactor(db): managers (#118)

---
+++ internal/db/managers.go
@@ -0,0 +1,166 @@
+// Copyright 2021 E99p1ant. All rights reserved.
+// Use of this source code is governed by Apache-2.0
+// license that can be found in the LICENSE file.
+
+package db
+
+import (
+	"context"
+	"crypto/sha256"
+	"crypto/subtle"
+	"fmt"
+
+	"github.com/pkg/errors"
+	"github.com/thanhpk/randstr"
+	"golang.org/x/crypto/pbkdf2"
+	"gorm.io/gorm"
+)
+
+var _ ManagersStore = (*managers)(nil)
+
+// Managers is the default instance of the ManagersStore.
+var Managers ManagersStore
+
+// ManagersStore is the persistent interface for managers.
+type ManagersStore interface {
+	// Authenticate validates name and password.
+	// It returns ErrBadCredentials when validate failed.
+	// The check account can't log in.
+	Authenticate(ctx context.Context, name, password string) (*Manager, error)
+	// Create creates a new manager and persists to database.
+	// It returns the manager when it created.
+	Create(ctx context.Context, opts CreateManagerOptions) (*Manager, error)
+	// Get returns all the managers.
+	Get(ctx context.Context) ([]*Manager, error)
+	// GetByID returns the manager with given id.
+	// It returns ErrManagerNotExists when not found.
+	GetByID(ctx context.Context, id uint) (*Manager, error)
+	// ChangePassword changes the manager's password with given id.
+	ChangePassword(ctx context.Context, id uint, newPassword string) error
+	// Update updates the manager with given id.
+	Update(ctx context.Context, id uint, opts UpdateManagerOptions) error
+	// DeleteByID deletes the manager with given id.
+	DeleteByID(ctx context.Context, id uint) error
+	// DeleteAll deletes all the managers.
+	DeleteAll(ctx context.Context) error
+}
+
+// NewManagersStore returns a ManagersStore instance with the given database connection.
+func NewManagersStore(db *gorm.DB) ManagersStore {
+	return &managers{DB: db}
+}
+
+// Manager represents the manager.
+type Manager struct {
+	gorm.Model
+
+	Name           string
+	Password       string
+	Salt           string
+	IsCheckAccount bool
+}
+
+// EncodePassword encodes password to safe format.
+func (m *Manager) EncodePassword() {
+	newPasswd := pbkdf2.Key([]byte(m.Password), []byte(m.Salt), 10000, 50, sha256.New)
+	m.Password = fmt.Sprintf("%x", newPasswd)
+}
+
+// ValidatePassword checks if given password matches the one belongs to the manager.
+func (m *Manager) ValidatePassword(password string) bool {
+	newManager := &Manager{Password: password, Salt: m.Salt}
+	newManager.EncodePassword()
+	return subtle.ConstantTimeCompare([]byte(m.Password), []byte(newManager.Password)) == 1
+}
+
+// getManagerSalt returns a random manager salt token.
+func getManagerSalt() string {
+	return randstr.String(10)
+}
+
+type managers struct {
+	*gorm.DB
+}
+
+func (db *managers) Authenticate(ctx context.Context, name, password string) (*Manager, error) {
+	var manager Manager
+	if err := db.WithContext(ctx).Model(&Manager{}).Where("name = ?", name).First(&manager).Error; err != nil {
+		return nil, ErrBadCredentials
+	}
+
+	// Check account can't log in.
+	if manager.IsCheckAccount || !manager.ValidatePassword(password) {
+		return nil, ErrBadCredentials
+	}
+	return &manager, nil
+}
+
+type CreateManagerOptions struct {
+	Name           string
+	Password       string
+	IsCheckAccount bool
+}
+
+var ErrManagerAlreadyExists = errors.New("manager already exits")
+
+func (db *managers) Create(ctx context.Context, opts CreateManagerOptions) (*Manager, error) {
+	var manager Manager
+	if err := db.WithContext(ctx).Model(&Manager{}).Where("name = ?", opts.Name).First(&manager).Error; err == nil {
+		return nil, ErrManagerAlreadyExists
+	} else if err != gorm.ErrRecordNotFound {
+		return nil, errors.Wrap(err, "get")
+	}
+
+	m := &Manager{
+		Name:           opts.Name,
+		Password:       opts.Password,
+		Salt:           getManagerSalt(),
+		IsCheckAccount: opts.IsCheckAccount,
+	}
+	m.EncodePassword()
+
+	return m, db.WithContext(ctx).Create(m).Error
+}
+
+func (db *managers) Get(ctx context.Context) ([]*Manager, error) {
+	var managers []*Manager
+	return managers, db.WithContext(ctx).Model(&Manager{}).Order("id ASC").Find(&managers).Error
+}
+
+var ErrManagerNotExists = errors.New("manager dose not exist")
+
+func (db *managers) GetByID(ctx context.Context, id uint) (*Manager, error) {
+	var manager Manager
+	if err := db.WithContext(ctx).Model(&Manager{}).Where("id = ?", id).First(&manager).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, ErrManagerNotExists
+		}
+		return nil, err
+	}
+
+	return &manager, nil
+}
+
+func (db *managers) ChangePassword(ctx context.Context, id uint, newPassword string) error {
+	var newManager Manager
+	newManager.Password = newPassword
+	newManager.EncodePassword()
+
+	return db.WithContext(ctx).Model(&Manager{}).Where("id = ?", id).Update("password", newManager.Password).Error
+}
+
+type UpdateManagerOptions struct {
+	IsCheckAccount bool
+}
+
+func (db *managers) Update(ctx context.Context, id uint, opts UpdateManagerOptions) error {
+	return db.WithContext(ctx).Model(&Manager{}).Where("id = ?", id).Update("is_check_account", opts.IsCheckAccount).Error
+}
+
+func (db *managers) DeleteByID(ctx context.Context, id uint) error {
+	return db.WithContext(ctx).Delete(&Manager{}, "id = ?", id).Error
+}
+
+func (db *managers) DeleteAll(ctx context.Context) error {
+	return db.WithContext(ctx).Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&Manager{}).Error
+}
---
+++ internal/db/managers_test.go
@@ -0,0 +1,319 @@
+// Copyright 2021 E99p1ant. All rights reserved.
+// Use of this source code is governed by Apache-2.0
+// license that can be found in the LICENSE file.
+
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+)
+
+func TestManagers(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+
+	t.Parallel()
+
+	db, cleanup := newTestDB(t)
+	err := db.AutoMigrate(&Manager{})
+	assert.Nil(t, err)
+	store := NewManagersStore(db)
+
+	for _, tc := range []struct {
+		name string
+		test func(t *testing.T, ctx context.Context, db *managers)
+	}{
+		{"Authenticate", testManagersAuthenticate},
+		{"Create", testManagersCreate},
+		{"Get", testManagersGet},
+		{"GetByID", testManagersGetByID},
+		{"ChangePassword", testManagersChangePassword},
+		{"Update", testManagersUpdate},
+		{"DeleteByID", testManagersDeleteByID},
+		{"DeleteAll", testManagersDeleteAll},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Cleanup(func() {
+				err := cleanup("managers")
+				if err != nil {
+					t.Fatal(err)
+				}
+			})
+			tc.test(t, context.Background(), store.(*managers))
+		})
+	}
+}
+
+func testManagersAuthenticate(t *testing.T, ctx context.Context, db *managers) {
+	want, err := db.Create(ctx, CreateManagerOptions{
+		Name:           "Vidar",
+		Password:       "123456",
+		IsCheckAccount: false,
+	})
+	assert.Nil(t, err)
+
+	// Correct credential.
+	got, err := db.Authenticate(ctx, "Vidar", "123456")
+	assert.Nil(t, err)
+	assert.Equal(t, want, got)
+
+	// Incorrect password.
+	_, err = db.Authenticate(ctx, "Vidar", "abcdef")
+	assert.Equal(t, ErrBadCredentials, err)
+
+	// Empty credential.
+	_, err = db.Authenticate(ctx, "", "")
+	assert.Equal(t, ErrBadCredentials, err)
+
+	_, err = db.Create(ctx, CreateManagerOptions{
+		Name:           "Checker",
+		Password:       "qwerty",
+		IsCheckAccount: true,
+	})
+	assert.Nil(t, err)
+
+	_, err = db.Authenticate(ctx, "Checker", "qwerty")
+	assert.Equal(t, ErrBadCredentials, err)
+}
+
+func testManagersCreate(t *testing.T, ctx context.Context, db *managers) {
+	got, err := db.Create(ctx, CreateManagerOptions{
+		Name:           "Vidar",
+		Password:       "123456",
+		IsCheckAccount: false,
+	})
+	assert.Nil(t, err)
+	assert.NotZero(t, got.Salt)
+	assert.NotZero(t, got.CreatedAt)
+	assert.NotZero(t, got.UpdatedAt)
+
+	got.CreatedAt = time.Time{}
+	got.UpdatedAt = time.Time{}
+
+	want := &Manager{
+		Model: gorm.Model{
+			ID: 1,
+		},
+		Name:           "Vidar",
+		Password:       "123456",
+		Salt:           got.Salt,
+		IsCheckAccount: false,
+	}
+	want.EncodePassword()
+
+	assert.Equal(t, want, got)
+
+	_, err = db.Create(ctx, CreateManagerOptions{
+		Name:           "Vidar",
+		Password:       "abcdef",
+		IsCheckAccount: true,
+	})
+	assert.Equal(t, ErrManagerAlreadyExists, err)
+}
+
+func testManagersGet(t *testing.T, ctx context.Context, db *managers) {
+	manager1, err := db.Create(ctx, CreateManagerOptions{
+		Name:           "Vidar",
+		Password:       "123456",
+		IsCheckAccount: false,
+	})
+	assert.Nil(t, err)
+
+	manager2, err := db.Create(ctx, CreateManagerOptions{
+		Name:           "Checker",
+		Password:       "abcdef",
+		IsCheckAccount: true,
+	})
+	assert.Nil(t, err)
+
+	manager3, err := db.Create(ctx, CreateManagerOptions{
+		Name:           "Cosmos",
+		Password:       "zxcvbn",
+		IsCheckAccount: false,
+	})
+	assert.Nil(t, err)
+
+	got, err := db.Get(ctx)
+
+	for k := range got {
+		got[k].CreatedAt = time.Time{}
+		got[k].UpdatedAt = time.Time{}
+	}
+
+	assert.Nil(t, err)
+
+	want := []*Manager{
+		{
+			Model: gorm.Model{
+				ID: 1,
+			},
+			Name:           "Vidar",
+			Password:       "123456",
+			Salt:           manager1.Salt,
+			IsCheckAccount: false,
+		},
+		{
+			Model: gorm.Model{
+				ID: 2,
+			},
+			Name:           "Checker",
+			Password:       "abcdef",
+			Salt:           manager2.Salt,
+			IsCheckAccount: true,
+		},
+		{
+			Model: gorm.Model{
+				ID: 3,
+			},
+			Name:           "Cosmos",
+			Password:       "zxcvbn",
+			Salt:           manager3.Salt,
+			IsCheckAccount: false,
+		},
+	}
+	want[0].EncodePassword()
+	want[1].EncodePassword()
+	want[2].EncodePassword()
+	assert.Equal(t, want, got)
+}
+
+func testManagersGetByID(t *testing.T, ctx context.Context, db *managers) {
+	_, err := db.Create(ctx, CreateManagerOptions{
+		Name:           "Vidar",
+		Password:       "123456",
+		IsCheckAccount: false,
+	})
+	assert.Nil(t, err)
+
+	got, err := db.GetByID(ctx, 1)
+	assert.Nil(t, err)
+
+	got.CreatedAt = time.Time{}
+	got.UpdatedAt = time.Time{}
+
+	want := &Manager{
+		Model: gorm.Model{
+			ID: 1,
+		},
+		Name:           "Vidar",
+		Password:       "123456",
+		Salt:           got.Salt,
+		IsCheckAccount: false,
+	}
+	want.EncodePassword()
+	assert.Equal(t, want, got)
+
+	// Get not exist manager.
+	got, err = db.GetByID(ctx, 2)
+	assert.Equal(t, ErrManagerNotExists, err)
+	want = (*Manager)(nil)
+	assert.Equal(t, want, got)
+}
+
+func testManagersChangePassword(t *testing.T, ctx context.Context, db *managers) {
+	manager, err := db.Create(ctx, CreateManagerOptions{
+		Name:           "Vidar",
+		Password:       "123456",
+		IsCheckAccount: false,
+	})
+	assert.Nil(t, err)
+
+	oldPassword := manager.Password
+
+	err = db.ChangePassword(ctx, 1, "newp@ssword")
+	assert.Nil(t, err)
+
+	manager, err = db.GetByID(ctx, 1)
+	assert.Nil(t, err)
+	newPassword := manager.Password
+
+	assert.NotEqual(t, oldPassword, newPassword)
+
+	err = db.ChangePassword(ctx, 2, "user_not_found")
+	assert.Nil(t, err)
+}
+
+func testManagersUpdate(t *testing.T, ctx context.Context, db *managers) {
+	manager, err := db.Create(ctx, CreateManagerOptions{
+		Name:           "Checker",
+		Password:       "abcdef",
+		IsCheckAccount: true,
+	})
+	assert.Nil(t, err)
+
+	err = db.Update(ctx, 1, UpdateManagerOptions{
+		IsCheckAccount: false,
+	})
+	assert.Nil(t, err)
+
+	got, err := db.GetByID(ctx, 1)
+	assert.Nil(t, err)
+	got.CreatedAt = time.Time{}
+	got.UpdatedAt = time.Time{}
+
+	want := &Manager{
+		Model: gorm.Model{
+			ID: 1,
+		},
+		Name:           "Checker",
+		Password:       "abcdef",
+		Salt:           manager.Salt,
+		IsCheckAccount: false,
+	}
+	want.EncodePassword()
+	assert.Equal(t, want, got)
+}
+
+func testManagersDeleteByID(t *testing.T, ctx context.Context, db *managers) {
+	_, err := db.Create(ctx, CreateManagerOptions{
+		Name:           "Vidar",
+		Password:       "123456",
+		IsCheckAccount: false,
+	})
+	assert.Nil(t, err)
+
+	err = db.DeleteByID(ctx, 1)
+	assert.Nil(t, err)
+
+	got, err := db.GetByID(ctx, 1)
+	assert.Equal(t, ErrManagerNotExists, err)
+	want := (*Manager)(nil)
+	assert.Equal(t, want, got)
+}
+
+func testManagersDeleteAll(t *testing.T, ctx context.Context, db *managers) {
+	_, err := db.Create(ctx, CreateManagerOptions{
+		Name:           "Vidar",
+		Password:       "123456",
+		IsCheckAccount: false,
+	})
+	assert.Nil(t, err)
+
+	_, err = db.Create(ctx, CreateManagerOptions{
+		Name:           "Checker",
+		Password:       "abcdef",
+		IsCheckAccount: true,
+	})
+	assert.Nil(t, err)
+
+	_, err = db.Create(ctx, CreateManagerOptions{
+		Name:           "Cosmos",
+		Password:       "zxcvbn",
+		IsCheckAccount: false,
+	})
+	assert.Nil(t, err)
+
+	err = db.DeleteAll(ctx)
+	assert.Nil(t, err)
+
+	got, err := db.Get(ctx)
+	assert.Nil(t, err)
+	want := []*Manager{}
+	assert.Equal(t, want, got)
+}
