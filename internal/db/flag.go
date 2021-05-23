ba6f8f5751f22d37f0073cc99fcbf125c7093381
E99p1ant <i@github.red>
2021-05-23 15:56:39 +0800

    refactor(db): flags (#121)

---
+++ internal/db/flag.go
@@ -0,0 +1,147 @@
+// Copyright 2021 E99p1ant. All rights reserved.
+// Use of this source code is governed by Apache-2.0
+// license that can be found in the LICENSE file.
+
+package db
+
+import (
+	"context"
+
+	"github.com/pkg/errors"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+)
+
+var _ FlagsStore = (*flags)(nil)
+
+// Flags is the default instance of the FlagsStore.
+var Flags FlagsStore
+
+// FlagsStore is the persistent interface for flags.
+type FlagsStore interface {
+	// BatchCreate creates flags and persists to database.
+	BatchCreate(ctx context.Context, opts CreateFlagOptions) error
+	// Get returns the flags.
+	Get(ctx context.Context, opts GetFlagOptions) ([]*Flag, error)
+	// Check checks the given flag.
+	// It returns ErrFlagNotExists when not found.
+	Check(ctx context.Context, flag string) (*Flag, error)
+	// DeleteAll deletes all the flags.
+	DeleteAll(ctx context.Context) error
+}
+
+// NewFlagsStore returns a FlagsStore instance with the given database connection.
+func NewFlagsStore(db *gorm.DB) FlagsStore {
+	return &flags{DB: db}
+}
+
+// Flag represents the flag which team submitted.
+type Flag struct {
+	gorm.Model
+
+	TeamID      uint `gorm:"uniqueIndex:gamebox_unique_idx"`
+	ChallengeID uint `gorm:"uniqueIndex:gamebox_unique_idx"`
+	GameBoxID   uint `gorm:"uniqueIndex:gamebox_unique_idx"`
+
+	Round uint
+	Value string
+}
+
+type flags struct {
+	*gorm.DB
+}
+
+type FlagMetadata struct {
+	GameBoxID uint
+	Round     uint
+	Value     string
+}
+
+type CreateFlagOptions struct {
+	Flags []FlagMetadata
+}
+
+func (db *flags) BatchCreate(ctx context.Context, opts CreateFlagOptions) error {
+	tx := db.WithContext(ctx).Begin()
+
+	gameBoxIDs := map[uint]struct{}{}
+	for _, flag := range opts.Flags {
+		gameBoxIDs[flag.GameBoxID] = struct{}{}
+	}
+
+	var err error
+	gameBoxesStore := NewGameBoxesStore(tx)
+	gameBoxSets := make(map[uint]*GameBox, len(gameBoxIDs))
+	for gameBoxID := range gameBoxIDs {
+		gameBoxSets[gameBoxID], err = gameBoxesStore.GetByID(ctx, gameBoxID)
+		if err != nil {
+			tx.Rollback()
+			if err == ErrGameBoxNotExists {
+				return err
+			}
+			return errors.Wrap(err, "get game box")
+		}
+	}
+
+	flags := make([]*Flag, 0, len(opts.Flags))
+	for _, flag := range opts.Flags {
+		flag := flag
+		gameBox := gameBoxSets[flag.GameBoxID]
+		flags = append(flags, &Flag{
+			TeamID:      gameBox.TeamID,
+			ChallengeID: gameBox.ChallengeID,
+			GameBoxID:   gameBox.ID,
+			Round:       flag.Round,
+			Value:       flag.Value,
+		})
+	}
+
+	if err := tx.Clauses(clause.OnConflict{
+		Columns:   []clause.Column{{Name: "team_id"}, {Name: "challenge_id"}, {Name: "game_box_id"}},
+		DoUpdates: clause.AssignmentColumns([]string{"value"}),
+	}).CreateInBatches(flags, len(flags)).Error; err != nil {
+		tx.Rollback()
+		return errors.Wrap(err, "batch create flag")
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
+
+type GetFlagOptions struct {
+	TeamID      uint
+	ChallengeID uint
+	GameBoxID   uint
+	Round       uint
+}
+
+func (db *flags) Get(ctx context.Context, opts GetFlagOptions) ([]*Flag, error) {
+	var flags []*Flag
+	return flags, db.WithContext(ctx).Model(&Flag{}).Where(&Flag{
+		TeamID:      opts.TeamID,
+		GameBoxID:   opts.GameBoxID,
+		ChallengeID: opts.ChallengeID,
+		Round:       opts.Round,
+	}).Find(&flags).Error
+}
+
+var ErrFlagNotExists = errors.New("flag does not find")
+
+func (db *flags) Check(ctx context.Context, flagValue string) (*Flag, error) {
+	var flag Flag
+	err := db.WithContext(ctx).Model(&Flag{}).Where("value = ?", flagValue).First(&flag).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, ErrFlagNotExists
+		}
+		return nil, errors.Wrap(err, "get")
+	}
+	return &flag, nil
+}
+
+func (db *flags) DeleteAll(ctx context.Context) error {
+	return db.WithContext(ctx).Session(&gorm.Session{AllowGlobalUpdate: true}).Delete(&Flag{}).Error
+}
